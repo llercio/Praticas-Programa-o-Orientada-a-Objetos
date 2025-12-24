1586a281447ee32754c02473066e573d63df9f58
llercio <lercioluiz1995@gmail.com>
2025-12-23 21:37:38 -0300

    first commit on this repo

---
+++ src/MainProduto.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void main() {
+        Produto gabinete = new Produto();
+        gabinete.setNome("Gabinete");
+        gabinete.setPreco(150);
+
+        gabinete.fichaProduto();
+        gabinete.aplicarDesconto(20);
+        System.out.println("Valor do produto com 20% de desconto: " + gabinete.getPreco());
+
+    }
+}
